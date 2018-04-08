7c1d21bd467469090b89d7a5d136b421cda3af9a
Mustafa Yusuf <mustafaaliasger.2014@vit.ac.in>
2018-04-08 14:23:16 +0530

    Initial Commit

---
+++ Roots/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Roots
+//
+//  Created by Mustafa Yusuf on 08/04/18.
+//  Copyright © 2018 Mustafa Yusuf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
